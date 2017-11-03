70f3ef152ecfc64e53ac053064b5f99de7de0697
shanemiracle <824116664@qq.com>
2017-11-03 20:13:39 +0800

    2level添加

--- public/template/static/js/index.js
+++ public/template/static/js/index.js
@@ -12,6 +12,48 @@ $(function() {
         }, 250);
     });
 
+    var table = $('#fileTable').DataTable({
+        "fnInfoCallback":function (oSettings,iStart,iEnd,iMax,iTotal,sPre) {
+
+        },
+        bLengthChange:false,
+        searching:true,
+        processing:true,
+        aaSorting:[
+            [1,'asc']
+        ],
+        bStateSave:true,
+        columns:[
+            {
+                // data:"file_name"
+                "sClass":"text-center",
+                // "data":{"data":"address"},
+                "data":{"data":""},
+                "render":function (data,type,full,meta) {
+                    // console.log(data.address);
+                    // console.log(data.file_name);
+                    return "<a href='" + data.address + "'" + "download='" + data.file_name + "'>" + data.file_name+ "</a>"
+                }
+            },
+            {
+                data:"remark"
+            },
+            {
+                data:"depart"
+            },
+            {
+                data:"size"
+            },
+            {
+                data:"create_user"
+            },
+            {
+                data:"create_time"
+            }
+        ]
+
+    });
+
     //树的渲染
     $.get('/api/apiQuaTreeListGet', function(res) {
         if (res.ret_code == 0) {
@@ -107,7 +149,7 @@ $(function() {
             }
         });
 
-    })
+    });
     $('.note-two').on('click',function(){
         var index = layer.open({
             type: 2,
@@ -119,7 +161,7 @@ $(function() {
             }
         });
 
-    })
+    });
     $('.note-three').on('click',function(){
         var index = layer.open({
             type: 2,
@@ -131,7 +173,7 @@ $(function() {
             }
         });
 
-    })
+    });
    
     //文件更新
     $('.update-file').on('click', function() {
@@ -147,27 +189,31 @@ $(function() {
 
     //文件列表展示
     function fileListShow() {
+        console.log("redraw file list");
         id = parent_id;
+        var url = '/api/apiQuaTreeFileListGet?id=' + id;
 
-        $.post('/api/apiQuaTreeFileListGet?id=' + id, function(res) {
-            var str = '',
-                len = res.data.length,
-                ret = res.data
-            for (var i = 0; i < len; i++) {
-                str += "<tr class='text-c c-666'>" +
-                    "<td>" + "<a href='" + ret[i].address + "'" + "download='" + ret[i].file_name + "'>" + ret[i].file_name + "</a>" + "</td>" +
-                    "<td>" + ret[i].depart + "</td>" +
-                    "<td>" + ret[i].create_time + "</td>" +
-                    "<td>" + ret[i].remark + "</td>" +
-                    "</tr>"
-            }
-            $('.table-content').html(str);
+        table.ajax.url(url).load(null,false);
 
-        })
+        // $.post('/api/apiQuaTreeFileListGet?id=' + id, function(res) {
+        //     var str = '',
+        //         len = res.data.length,
+        //         ret = res.data;
+        //     for (var i = 0; i < len; i++) {
+        //         str += "<tr class='text-c c-666'>" +
+        //             "<td>" + "<a href='" + ret[i].address + "'" + "download='" + ret[i].file_name + "'>" + ret[i].file_name + "</a>" + "</td>" +
+        //             "<td>" + ret[i].depart + "</td>" +
+        //             "<td>" + ret[i].create_time + "</td>" +
+        //             "<td>" + ret[i].remark + "</td>" +
+        //             "</tr>"
+        //     }
+        //     $('.table-content').html(str);
+
+        // }
+        // )
     }
 
     function redraw(){
-
         $.get('/api/apiQuaTreeListGet', function(res) {
          if (res.ret_code == 0) {
              $('#container').jstree(true).settings.core.data=res.data;
@@ -176,7 +222,9 @@ $(function() {
          } else {
              layer.msg(res.ret_desc);
          }
-    })
+    });
+
+
   }
 
 
