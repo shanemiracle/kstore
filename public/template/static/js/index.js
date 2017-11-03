e527ad07a0147388e291a2a964a4b451b64767da
jinjidecoco <18895627215@163.com>
2017-11-03 18:36:42 +0800

    coco

--- public/template/static/js/index.js
+++ public/template/static/js/index.js
@@ -1,114 +1,183 @@
 $(function() {
-  //实现搜索功能
+    //实现搜索功能
     var to = false;
-    var parent_id ='';
-    var  id = '';
+    var parent_id = '';
+    var id = '';
     $('#searchMenu').keyup(function() {
         if (to) { clearTimeout(to); }
         to = setTimeout(function() {
             var v = $('#searchMenu').val();
-            console.log(v);
+            // console.log(v);
             $('#container').jstree(true).search(v);
         }, 250);
     });
 
     //树的渲染
     $.get('/api/apiQuaTreeListGet', function(res) {
-        console.log(res);
-        $('#container').jstree({
-            'core': {
-                'data': res.data
-            },
-            // types:{
-            //     "default" : {
-            //       "icon" : "icon iconfont icon-loucengyi"
-            //   } 
-            // },
-            // "state": { "key": "state_demo" },
-            "plugins": ["sort", "state", "search", 'wholerow', "types"]
-        });
-    })
+        if (res.ret_code == 0) {
+            $('#container').jstree({
+                'core': {
+                    'data': res.data
+                },
+                // types:{
+                //     "default" : {
+                //       "icon" : "icon iconfont icon-loucengyi"
+                //   } 
+                // },
+                // "state": { "key": "state_demo" },
+    "plugins": ["sort", "state", "search", 'wholerow', "types"]
+            });
+
+        } else {
+            layer.msg(res.ret_desc);
+        }
+    });
 
     $('#container').on('select_node.jstree', function(e, data) {
-        console.log(data);
+        // console.log(data);
         console.log(data.selected);
-           parent_id=data.selected;
+        parent_id = data.selected;
         var selectedeep = (data.selected[0]);
         var deep = selectedeep.substring(0, 1);
         $('.contents .filter').eq(deep).show().siblings().hide();
 
         fileListShow();
     });
-    
 
+    
+      //下一层文件弹窗
     $('.pop-one').on('click', function() {
         var index = layer.open({
             type: 2,
-            title: '创建第一层',
+            title: '创建下一层页面',
             area: ['720px', '510px'],
-            content: '/tree/index?parent_id=' + parent_id
+            content: '/tree/index?parent_id=' + parent_id,
+            end:function(){
+                redraw();
+            }
         });
     });
-
-    $('.edit-first').on('click', function() {
-        var index = layer.open({
-            type: 2,
-            title: '编辑第一层',
-            area: ['480px', '360px'],
-            content: '/tree/editfirst'
-
-        });
-    })
+  
     $('.pop-two').on('click', function() {
         var index = layer.open({
             type: 2,
-            title: '创建第二页面',
-            area: ['720px', '510px'],
-            content: '/tree/popsecond?parent_id=' +parent_id
-
+            title: '创建2一层页面',
+            area: ['560px', '320px'],
+            content: '/tree/popsecond?parent_id=' + parent_id,
+            end:function(){
+                redraw();
+            }
         });
-    })
+    });
+
     $('.pop-three').on('click', function() {
         var index = layer.open({
             type: 2,
-            title: '创建第三页面',
-            area: ['700px', '420px'],
-            content: '/tree/popthird'
+            title: '创建下一层页面',
+            area: ['720px', '510px'],
+            content: '/tree/popthird?parent_id=' + parent_id,
+            end:function(){
+                redraw();
+            }
 
         });
-    })
+    });
     $('.pop-four').on('click', function() {
         var index = layer.open({
             type: 2,
-            title: '创建页面',
-            area: ['700px', '420px'],
-            content: '/tree/popfourth'
-
+            title: '创建下一层页面',
+            area: ['720px', '510px'],
+            content: '/tree/popfourth?parent_id=' + parent_id,
+            end:function(){
+                redraw();
+            }
         });
     })
 
-    
+
+      //记录文件弹窗
+    $('.note-one').on('click',function(){
+        var index = layer.open({
+            type: 2,
+            title: '创建记录页面',
+            area: ['720px', '510px'],
+            content: '/tree/recordOne?parent_id=' + parent_id,
+            end:function(){
+                redraw();
+            }
+        });
+
+    })
+    $('.note-two').on('click',function(){
+        var index = layer.open({
+            type: 2,
+            title: '创建记录页面',
+            area: ['720px', '510px'],
+            content: '/tree/recordTwo?parent_id=' + parent_id,
+            end:function(){
+                redraw();
+            }
+        });
+
+    })
+    $('.note-three').on('click',function(){
+        var index = layer.open({
+            type: 2,
+            title: '创建记录页面',
+            area: ['720px', '510px'],
+            content: '/tree/recordThree?parent_id=' + parent_id,
+            end:function(){
+                redraw();
+            }
+        });
+
+    })
+   
+    //文件更新
+    $('.update-file').on('click', function() {
+        var index = layer.open({
+            type: 2,
+            title: '更新第一层',
+            area: ['600px', '420px'],
+            content: '/tree/editfirst?parent_id=' + parent_id
+
+        });
+    });
+
+
     //文件列表展示
-    function fileListShow(){
-        id=parent_id;
-      
-        $.post('/api/apiQuaTreeFileListGet?id='+ id ,  function(res){
-          var str='',
-              len=res.data.length,
-              ret=res.data
-          for(var i=0;i<len;i++){
+    function fileListShow() {
+        id = parent_id;
 
-            str+="<td>" + "<a href='" + ret[i].address+"'" + "download='"+ret[i].file_name+"'>" + ret[i].file_name +"</a>" +"</td>" +
-                 "<td>" + ret[i].depart +"</td>" +
-                 "<td>" + ret[i].create_time +"</td>"+
-                 "<td>" + ret[i].remark +"</td>"
-          }
+        $.post('/api/apiQuaTreeFileListGet?id=' + id, function(res) {
+            var str = '',
+                len = res.data.length,
+                ret = res.data
+            for (var i = 0; i < len; i++) {
+                str += "<tr class='text-c c-666'>" +
+                    "<td>" + "<a href='" + ret[i].address + "'" + "download='" + ret[i].file_name + "'>" + ret[i].file_name + "</a>" + "</td>" +
+                    "<td>" + ret[i].depart + "</td>" +
+                    "<td>" + ret[i].create_time + "</td>" +
+                    "<td>" + ret[i].remark + "</td>" +
+                    "</tr>"
+            }
+            $('.table-content').html(str);
 
-          $('.table-content').html(str);
-
-        })   
-        
+        })
     }
+
+    function redraw(){
+
+        $.get('/api/apiQuaTreeListGet', function(res) {
+         if (res.ret_code == 0) {
+             $('#container').jstree(true).settings.core.data=res.data;
+             $('#container').jstree(true).refresh();
+
+         } else {
+             layer.msg(res.ret_desc);
+         }
+    })
+  }
 
 
 
