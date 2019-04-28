bbb1c98d2d0cfca9a238c99f47a4821775bc253b
shanemiracle <824116664@qq.com>
2019-04-28 09:34:40 +0800

    Update database.php

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '115.236.69.122',
+    'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'wk_kstore',
     // 用户名
     'username'        => 'admin',
     // 密码
-    'password'        => '2ws4rf6yh',
+    'password'        => '123456',
     // 端口
     'hostport'        => '',
     // 连接dsn
